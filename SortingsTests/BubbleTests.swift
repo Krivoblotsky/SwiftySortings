dedb1c614c8af422c0a99623df413fa5e0297698
Deszip <raf.rafiki@gmail.com>
2016-02-21 13:03:46 +0200

    Tests added

---
+++ SortingsTests/BubbleTests.swift
@@ -0,0 +1,23 @@
+//
+//  BubbleTests.swift
+//  Sortings
+//
+//  Created by Deszip on 21/02/16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+import XCTest
+
+class BubbleTests: SortingTests {
+
+    override func setUp() {
+        super.setUp()
+        
+        sorting = Bubble()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
---
+++ SortingsTests/ShellTests.swift
@@ -0,0 +1,23 @@
+//
+//  ShellTests.swift
+//  Sortings
+//
+//  Created by Deszip on 21/02/16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+import XCTest
+
+class ShellTests: SortingTests {
+
+    override func setUp() {
+        super.setUp()
+        
+        sorting = Shell()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
